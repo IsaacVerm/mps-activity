75a0fc8d0a0a6ec9eeb12fb88fc3fb790b43bb79
IsaacVerm <isaacverm@gmail.com>
2019-10-25 16:16:17 +0200

    add E2E example saving meeting notes

--- save-meeting-notes.js
+++ save-meeting-notes.js
@@ -1,10 +1,20 @@
 var get = require('./get-parliament-api');
 var extract = require('./extract');
+var csv = require('./write-to-csv');
 
-get.getMeetings(get.domain, '2016', '5', '1053519').then(meetings => {
-  const meeting = meetings.data.items[0];
-  console.log('persons present');
-  console.log(extract.extractPersonsPresentDetails(meeting));
-  console.log('meeting details');
-  console.log(extract.extractMeetingDetails(meeting));
+get.getMeetings(get.domain, '2016', '5', '1053519').then(meetingsResponse => {
+  // get meetings
+  const meetings = meetingsResponse.data.items;
+
+  meetings.forEach(meeting => {
+    // extract persons present and meeting details
+    const personsPresentDetails = extract.extractPersonsPresentDetails(meeting);
+    const meetingDetails = extract.extractMeetingDetails(meeting);
+
+    // create records to save
+    const meetingRecords = csv.createMeetingRecords(personsPresentDetails, meetingDetails);
+
+    // save records to file
+    csv.writeMeetingsToCsv(meetingRecords);
+  });
 });
--- write-to-csv.js
+++ write-to-csv.js
@@ -1,18 +1,47 @@
-function createMeetingsArray(personDetails, meetingDetails) {
-  const records = [];
-  personsPresent.forEach(person => {
-    records.push({ meetingId: meetingId, person: person });
-  });
-}
+module.exports = {
+  createMeetingRecords: function(personsPresentDetails, meetingDetails) {
+    const meetingRecords = [];
+    personsPresentDetails.forEach(person => {
+      meetingRecords.push({
+        meetingId: meetingDetails.id,
+        commissionName: meetingDetails.commissionName,
+        commissionId: meetingDetails.commissionId,
+        start: meetingDetails.start,
+        end: meetingDetails.end,
+        description: meetingDetails.description,
+        personId: person.id,
+        personFirstName: person.firstName,
+        personLastName: person.lastName,
+        partyName: person.partyName,
+        partyId: person.partyId,
+        gender: person.gender,
+      });
+    });
+    return meetingRecords;
+  },
+  writeMeetingsToCsv: function(meetingRecords) {
+    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+    const csvWriter = createCsvWriter({
+      path: 'meetings.csv',
+      header: [
+        { id: 'meetingId', title: 'meetingId' },
+        { id: 'commissionName', title: 'commissionName' },
+        { id: 'commissionId', title: 'commissionId' },
+        { id: 'start', title: 'start' },
+        { id: 'end', title: 'end' },
+        { id: 'description', title: 'description' },
+        { id: 'personId', title: 'personId' },
+        { id: 'personFirstName', title: 'personFirstName' },
+        { id: 'personLastName', title: 'personLastName' },
+        { id: 'partyName', title: 'partyName' },
+        { id: 'partyId', title: 'partyId' },
+        { id: 'gender', title: 'gender' },
+      ],
+      append: true,
+    });
 
-function writeMeetingToCsv(meetingsArray) {
-  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-  const csvWriter = createCsvWriter({
-    path: 'meetings.csv',
-    header: [{ id: 'meetingId', title: 'meetingId' }, { id: 'person', title: 'person' }],
-  });
-
-  csvWriter.writeRecords(records).then(() => {
-    console.log(`meeting with id ${meetingId} written to meetings.csv`);
-  });
-}
+    csvWriter.writeRecords(meetingRecords).then(() => {
+      console.log(`meeting with id ${meetingRecords[0].meetingId} written to meetings.csv`);
+    });
+  },
+};
